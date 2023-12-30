5df8d7049621afada88603d16f2074b987ea4dad
CDiala <dialachibuzo@yahoo.com>
2023-12-30 05:32:28 +0100

    chore: initial commit
    Create a new react project with vite and install the following packages: axios, formik, yup, tailwindCSS, SCSS, headlessUI.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="main-box">
+        <p className="text-3xl font-bold underline sub-text">Hello world!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
