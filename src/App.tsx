614201ec1eae509d2fefef1ca7479a54560a7ac3
CDiala <dialachibuzo@yahoo.com>
2023-12-31 18:10:40 +0100

    fix: Modify button component
    Add ButtonProps interface, restructure and restyle button component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import "./App.scss";
+import { Button } from "./components";
+import Input from "./components/Input/Input";
 
 function App() {
   return (
-    <>
-      <div className="main-box">
-        <p className="text-3xl font-bold underline sub-text">Hello world!</p>
-      </div>
-    </>
+    <div className="flex flex-col gap-2 m-1 w-96">
+      <Input type={"email"} placeholder={"Email"} size={"small"}></Input>
+      <Input type={"password"} placeholder={"Password"} size={"large"}></Input>
+      <div></div>
+      <Button block size={"lg"} theme={"primary"}>
+        Log In
+      </Button>
+      <Button block size={"sm"} theme={"secondary"}>
+        Reset
+      </Button>
+      <Button block size={"sm"} theme={"danger"}>
+        Log In
+      </Button>
+      <Button block size={"sm"} theme={"primary-outline"}>
+        Log In
+      </Button>
+      <Button size={"sm"} theme={"secondary-outline"}>
+        Reset
+      </Button>
+      <Button size={"sm"} theme={"danger-outline"}>
+        BLACKLIST USER
+      </Button>
+      <Button size={"sm"} theme={"primary-outline"}>
+        ACTIVATE USER
+      </Button>
+    </div>
   );
 }
 
